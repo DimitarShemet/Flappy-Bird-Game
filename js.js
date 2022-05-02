ec119bbf5e96c5f6bf1df182b2123e52ed058a1f
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-05-02 14:16:15 +0300

    added speed

--- js.js
+++ js.js
@@ -4,20 +4,27 @@ canvas.width=innerWidth
 canvas.height=innerHeight
 let mapWidth=canvas.width
 let mapHeight=canvas.height
+ 
+ let ctx=canvas.getContext("2d")
 
+  //Рисуем трубу
+  let tube=document.getElementById('tube')
+  let tubeWidth=mapWidth/15
+   let tubeHeight=mapHeight/2.5
+   let tubeX=mapWidth
+ 
+  
 
 
 setInterval(render,1000/60)
 function render(){
-    //рисуем карту
-let ctx=canvas.getContext("2d")
-ctx.fillStyle="lightblue"
-ctx.fillRect(0,0, mapWidth, mapHeight)
- //Рисуем трубу
- let tube=document.getElementById('tube')
- let tubeWidth=mapWidth/15
-  let tubeHeight=mapHeight/2.5
- ctx.drawImage(tube, 50,0, tubeWidth, tubeHeight);
+     //рисуем карту
+    ctx.fillStyle="lightblue"
+    ctx.fillRect(0,0, mapWidth, mapHeight)
+     //Рисуем трубу
+    ctx.drawImage(tube, tubeX,0, tubeWidth, tubeHeight);
+    tubeX=tubeX-1
+ 
  console.log("1")
 }
 
