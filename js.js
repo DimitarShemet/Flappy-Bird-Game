455c552d7ef4c13325585c17eb8de5e6e54daf61
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-05-02 13:59:35 +0300

    fixed js

--- js.js
+++ js.js
@@ -4,15 +4,25 @@ canvas.width=innerWidth
 canvas.height=innerHeight
 let mapWidth=canvas.width
 let mapHeight=canvas.height
+
+
+
+setInterval(render,1000/60)
+function render(){
+    //рисуем карту
 let ctx=canvas.getContext("2d")
 ctx.fillStyle="lightblue"
 ctx.fillRect(0,0, mapWidth, mapHeight)
  //Рисуем трубу
  let tube=document.getElementById('tube')
- 
  let tubeWidth=mapWidth/15
   let tubeHeight=mapHeight/2.5
-
  ctx.drawImage(tube, 50,0, tubeWidth, tubeHeight);
- 
+ console.log("1")
 }
+
+
+
+
+
+ }
