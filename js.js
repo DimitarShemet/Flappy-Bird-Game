ad14956fee9c7da3a594e7454e15d87733a49d78
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-05-02 12:46:35 +0300

    added tube

---
+++ js.js
@@ -0,0 +1,22 @@
+let canvas=document.getElementById('canvas')
+canvas.width=innerWidth
+canvas.height=innerHeight
+let mapWidth=canvas.width
+let mapHeight=canvas.height
+let ctx=canvas.getContext("2d")
+ctx.fillStyle="lightblue"
+ctx.fillRect(0,0, mapWidth, mapHeight)
+ //Рисуем трубу
+ let tube=document.getElementById('tube')
+ 
+ tube.width=64
+ tube.height=400
+ window.onload=function(){
+ ctx.drawImage(tube, 50,0,mapWidth/35, mapHeight/2.5);
+ 
+ }
+setInterval(render,1000/60)
+function render(){
+
+
+}
