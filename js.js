f1688dcd02c3855c7460d02516f466577285e3dc
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-05-02 16:55:57 +0300

    fix

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
-window.onload=function(){
+window.onerror=function(msg, url, line){
+    alert('Message: '+msg+ "\nurl: "+ url + "\n Line : " + line)
+    return true
+
+}
+
+
 let canvas=document.getElementById('canvas')
 canvas.width=innerWidth
 canvas.height=innerHeight
@@ -7,27 +13,34 @@ let mapHeight=canvas.height
  
  let ctx=canvas.getContext("2d")
 
-  //Рисуем трубу
+  //Настройки труб
   let tube=document.getElementById('tube')
   let tubeWidth=mapWidth/15
-   let tubeHeight=mapHeight/2.5
+   let tubeHeight=mapHeight/2
    let tubeX=mapWidth
  let speed=1
+ let distance=innerHeight/5
+  // Настройки птицы
+   let bird=document.getElementById('bird')
+   let birdX=50
+   let birdY=100
+   let birdWith=60
+   let birdHeight=150
+
   
-
-
 setInterval(render,1000/60)
 function render(){
-     //рисуем карту
+     //Рисуем карту
     ctx.fillStyle="lightblue"
     ctx.fillRect(0,0, mapWidth, mapHeight)
-     //Рисуем трубу
-    ctx.drawImage(tube, tubeX,0, tubeWidth, tubeHeight);
-    ctx.drawImage(tube,tubeX,innerHeight-tubeHeight,tubeWidth, tubeHeight);
+     //Рисуем трубы
+    ctx.drawImage(tube,tubeX, 0, tubeWidth, tubeHeight);
+    ctx.drawImage(tube,tubeX,tubeHeight+distance,tubeWidth, tubeHeight);
     tubeX=tubeX-1-speed
- 
+    // Рисуем птицу
+     
 
-}
+
 
 
 
