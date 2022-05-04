1afa55961174ea0b65561c98488309680fe642b0
Dimitar Shemet <85831324+DimitarShemet@users.noreply.github.com>
2022-05-04 21:29:25 +0300

    fix speed

--- js.js
+++ js.js
@@ -40,7 +40,7 @@ function birdUp(){
 canvas.addEventListener("touchstart",birdup,false)
  
 function birdup(){
-   gravity=gravity-mapHeight/10000
+   gravity=gravity-mapHeight/670
 }    
    // функция сброса
    function reset(){
